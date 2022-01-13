3f3da6940edcc197fb0a88f8a4dce6baa628ca3f
hafizsem <hafiz.lampard20@gmail.com>
2022-01-13 17:16:22 +0800

    Revert "Update test project.py"
    This reverts commit 0337f09e6ca4d2f8f9509fdd17070088775134af.

--- test project.py
+++ test project.py
@@ -6,57 +6,38 @@ import tkinter as tk
 from tkinter import*
 window=tk.Tk()
 window.title("Library Catalogue")
+
+# my_menu=Menu()
+# window.config(menu=my_menu)
+#
+# filemenu=Menu(my_menu)
+# my_menu.add_cascade(label="Select",menu=filemenu)
+# filemenu.add_command(label="Borrow")
+# filemenu.add_command(label="Return")
+# filemenu.add_separator()
+# filemenu.add_command(label="Exit")
+
 window.geometry("1000x1000")
 
 frame=tk.Frame(window)
 frame.place(relx=0.2,rely=0.2,relheight=0.6,relwidth=0.6)
-
-#input
-
 
 def page1():
     label=tk.Label(frame,text='What book you want to borrow?')
     label.place(relx=0.3,rely=0.4)
 
 def page2():
-    label2=tk.Label(frame,text='Return the book')
-    label2.place(relx=0.4,rely=0.5)
-
-L1 = Label(window, text = "Name : ")
-L1.place(x = 10,y = 10, width ='50')
-E1 = Entry(window, bd = 3, width ='50')
-E1.place( x = 60,y = 10)
-
-L2 = Label(window,text = "Book ID :  ")
-L2.place(x = 10,y = 50, width = '50')
-E2 = Entry(window,bd = 3, width ='50')
-E2.place(x = 60,y = 50)
-
-B1 = Button(window, text = "Borrow", command=page1)
-B1.place(x = 225, y = 100, width = '50', anchor ="c")
+    label=tk.Label(frame,text='Return the book')
+    label.place(relx=0.3,rely=0.4)
 
 
-B2 = Button(window, text = "Return", command=page2)
-B2.place(x = 225, y = 130, width = '50', anchor ="c")
-window.geometry("250x250+10+10")
 
+bt=tk.Button(window,text='Borrow',command=page1)
+bt.grid(column=0,row=0)
 
-# def page1():
-#     label=tk.Label(frame,text='What book you want to borrow?')
-#     label.place(relx=0.3,rely=0.4)
-#
-# def page2():
-#     label=tk.Label(frame,text='Return the book')
-#     label.place(relx=0.3,rely=0.4)
-#
-#
-#
-# bt=tk.Button(window,text='Borrow',command=page1)
-# bt.grid(column=0,row=0)
-#
-# bt1=tk.Button(window,text='Return',command=page2)
-# bt1.grid(row=0,column=1)
-#
+bt1=tk.Button(window,text='Return',command=page2)
+bt1.grid(row=0,column=1)
+
 
 
 window.mainloop()
